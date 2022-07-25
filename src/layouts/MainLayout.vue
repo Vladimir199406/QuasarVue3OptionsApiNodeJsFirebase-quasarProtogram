aba5077ae86f5ad69ad5dab1e5444a74a593f314
vo <vovasso550@rambler.ru>
2022-07-25 16:03:46 +0300

    app-created

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v</div>
+      </q-toolbar>
+    </q-header>
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+export default {
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  data() {
+    return {
+
+    }
+  }
+
+}
+</script>
