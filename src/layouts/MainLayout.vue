5b23e8002adf4c015da4b64cdb610d0f817ea0fe
vo <vovasso550@rambler.ru>
2022-07-25 18:55:35 +0300

    header layout upadte

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,34 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header reveal>
-      <q-toolbar>
-        <q-btn flat @click="drawerLeft = !drawerLeft" round dense icon="menu" />
-        <q-toolbar-title>Quasar Protogram</q-toolbar-title>
+    <q-header>
+      <q-toolbar class="constarain">
+        <q-btn
+          class="large-screen-only q-mr-sm"
+          to="/camera"
+          flat
+          round
+          icon="eva-camera-outline"
+          size="25px"
+          dense
+          padding="5px"
+        >        
+        </q-btn>
+        <q-toolbar-title class="text-main-headers">Quasar Protogram</q-toolbar-title>
+        <q-btn
+          class="large-screen-only"
+          to="/"
+          flat
+          round
+          icon="eva-home-outline"
+          size="25px"
+          dense
+          padding="5px"
+        >        
+        </q-btn>
       </q-toolbar>
     </q-header>
 
-    <q-footer class="bg-white" bordered>
+    <q-footer class="bg-white small-screen-only" bordered>
         <q-tabs
           indicator-color="transparent"
           class="text-grey-10"
@@ -44,6 +65,24 @@ export default {
 </script>
 
 <style lang="scss">
+  .q-toolbar__title {
+    @media (min-width: $breakpoint-sm-min) {
+      height: 70px;
+    }
+  }
+
+  .q-header {
+    .q-toolbar__title {
+      text-align: center;
+      font-size: 25px;
+
+      @media (min-width: $breakpoint-xs-max) {
+        text-align: left;
+        padding-top: 10px;
+      }
+    }
+  }
+
   .q-footer {
     .q-tab__icon {
       font-size: 30px;
