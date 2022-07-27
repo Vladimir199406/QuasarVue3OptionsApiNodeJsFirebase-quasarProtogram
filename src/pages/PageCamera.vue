216144cab6018cd577a04fcb94c8fdf5a8e19574
vo <vovasso550@rambler.ru>
2022-07-27 16:51:29 +0300

    capture img

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,11 +1,45 @@
 <template>
   <q-page class="constarain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <video class="full-width" autoplay playsinkline ref="video"></video>
-      <canvas class="full-width" height="240" ref="canvas"></canvas>
+      <video 
+        class="full-width" 
+        autoplay 
+        playsinkline 
+        ref="video" 
+        v-show="!imageCaptured"
+      >
+      </video>
+      <canvas 
+        class="full-width" 
+        height="240" 
+        ref="canvas" 
+        v-show="imageCaptured"
+      >
+      </canvas>
     </div>
     <div class="text-center q-pa-md">
-      <q-btn size="lg" round color="grey-10" icon="eva-camera" @click="captureImage"/>
+      <q-btn
+        v-if="hasCameraSupport"
+        size="lg" 
+        round 
+        color="grey-10" 
+        icon="eva-camera" 
+        @click="captureImage" 
+      />
+
+      <q-file 
+        v-else
+        label="Choose an Image"
+        outlined 
+        v-model="imageUpload"
+        accept="image/*"
+        @input="captureImageFallback"
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
+
     </div>
     <div class="row justify-center q-ma-md">
       <q-input filled v-model="post.caption" label="Caption" class="col-12 standart-margin-bottom" />
@@ -34,24 +68,76 @@ export default {
         location: '',
         photo: null,
         date: Date.now(),
-      }
+      },
+      imageCaptured: false,
+      hasCameraSupport: true,
+      imageUpload: [],
     }
   },
   methods: {
     initCamera() {
       navigator.mediaDevices.getUserMedia({
         video: true,
-      }).then(stream => {
+      })
+      .then(stream => {
         this.$refs.video.srcObject = stream;
+      })
+      .catch(error => {
+        this.hasCameraSupport = false;
       });
     },
+  
     captureImage() {
       const video = this.$refs.video;
       const canvas = this.$refs.canvas;
+
       canvas.width = video.getBoundingClientRect().width;
       canvas.height = video.getBoundingClientRect().height;
       const context = canvas.getContext('2d');
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+    },
+  
+    captureImageFallback(inputEvent) {
+      this.post.photo = inputEvent.path[0].files[0];
+
+      const canvas = this.$refs.canvas;
+      const context = canvas.getContext('2d');
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        let img = new Image();
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          this.imageCaptured = true;
+        }
+        img.src = event.target.result;
+      }
+      reader.readAsDataURL(this.post.photo);
+    },
+  
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      const byteString = atob(dataURI.split(',')[1]);
+      // separate out the mime component
+      const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+      // write the bytes of the string to an ArrayBuffer
+      const ab = new ArrayBuffer(byteString.length);
+      // create a view into the buffer
+      const ia = new Uint8Array(ab);
+      // set the bytes of the buffer to the correct values
+  
+      for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      const blob = new Blob([ab], { type: mimeString });
+      return blob;
     }
   },
   mounted() {
